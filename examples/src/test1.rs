5e7d9316860e1be9392796d533eb2810d1002711
jeremyletang <letang.jeremy@gmail.com>
2016-06-28 00:20:17 +0100

    first experiments

--- examples/src/test1.rs
+++ examples/src/test1.rs
@@ -1,9 +1,27 @@
 #![feature(custom_derive, plugin)]
 #![plugin(rpc_macros)]
+#![allow(unused_imports, unused_variables, dead_code)]
 
-#[derive(Service)]
+pub trait JsonConvertible {}
+impl JsonConvertible for i32 {}
+impl JsonConvertible for f32 {}
+
+pub trait ProtoConvertible {}
+impl ProtoConvertible for i32 {}
+impl ProtoConvertible for f32 {}
+
+// #[rpc_service(JsonConvertible, ProtoConvertible)]
 struct Test {
-    i: i32
+    i: i32,
 }
 
-fn main(){}
+#[rpc_service(JsonConvertible, ProtoConvertible)]
+impl Test {
+    fn hello(&mut self, i: i32, j: f32) {}
+    fn world(&mut self, i: i32, j: f32) {}
+}
+
+fn main(){
+    let t = Test{i: 42};
+    println!("{}", t.list());
+}
--- rpc_codegen/src/lib.rs
+++ rpc_codegen/src/lib.rs
@@ -2,25 +2,50 @@
 #![plugin(quasi_macros)]
 #![cfg_attr(feature = "clippy", feature(plugin))]
 #![cfg_attr(feature = "clippy", plugin(clippy))]
+#![allow(unused_imports, unused_variables)]
 
 extern crate syntax;
 extern crate rustc_plugin;
+extern crate quasi;
 
-use syntax::ast::{self, Ident, MetaItem};
+use syntax::ast::*;
+// use syntax::ast::{self, Ident, MetaItem};
 use syntax::codemap::Span;
 use syntax::ext::base::{Annotatable, ExtCtxt};
 use syntax::ptr::P;
 
-fn expand_derive_service(cx: &mut ExtCtxt,
+fn expand_rpc_service(cx: &mut ExtCtxt,
     span: Span,
     meta_item: &MetaItem,
     annotatable: &Annotatable,
     push: &mut FnMut(Annotatable)
-) {}
+) {
+    match annotatable {
+        &Annotatable::Item(ref i) => {
+            match &(*i).node {
+                &ItemKind::Impl(_, _, _, _, ref ty, ref items) => {
+                    println!("this is an impl");
+                    println!("TyKind: {:?}", (*ty).node);
+                    println!("Items: {:?}", items);
+                    let impl_item = quote_item!(cx,
+                        impl Test {
+                            pub fn list(&self) -> String{
+                                return "mouhahaha".to_string();
+                            }
+                        }
+                    ).unwrap();
+                    push(Annotatable::Item(impl_item));
+                },
+                _ => cx.span_err((*i).span, "#[rpc_service(...)] may only be applied to struct or enum impls"),
+            }
+        },
+        _ => cx.span_err(span, "#[rpc_service(...)] may only be applied to struct or enum impls")
+    }
+}
 
 pub fn register(reg: &mut rustc_plugin::Registry) {
     reg.register_syntax_extension(
-        syntax::parse::token::intern("derive_Service"),
+        syntax::parse::token::intern("rpc_service"),
         syntax::ext::base::MultiDecorator(
-            Box::new(expand_derive_service)));
+            Box::new(expand_rpc_service)));
 }
