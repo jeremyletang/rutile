7f3184347ace227079ff9138bf03d1013ee184ea
jeremyletang <letang.jeremy@gmail.com>
2016-06-28 08:45:56 +0100

    add two struct definitions

--- rpc/src/lib.rs
+++ rpc/src/lib.rs
@@ -1,5 +1,15 @@
 
+use std::collections::HashMap;
+
+type Context = HashMap<String, String>;
+
+struct Message {
+    method: String,
+    body: String,
+    id: i64
+}
+
 pub trait Service {
     fn rpc_service_name(&self) -> &'static str;
-    fn serve_rpc_request(&mut self, i: i32) -> bool;
+    fn serve_rpc_request(&mut self, i: Context, ) -> bool;
 }
