a82f612a7d09b3ee5be4a6111d212e0f060fcf3f
jeremy <jeremy@piemapping.com>
2016-07-22 13:47:07 +0100

    add content-type checking

--- rpc/src/transport/http_transport.rs
+++ rpc/src/transport/http_transport.rs
@@ -5,8 +5,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-
-use hyper::header::ContentLength;
+use hyper::header::{ContentType, ContentLength};
 use hyper::method::Method;
 use hyper::net::HttpListener;
 use hyper::server::{Server, Listening, Request, Response, Fresh, Handler};
@@ -80,24 +79,54 @@ impl Transport for HttpTransport {
 
 pub struct HttpHandler {
     services: Vec<Box<Service>>,
+    content_types: Vec<ContentType>,
+
 }
 
 impl HttpHandler {
     pub fn new(services: Vec<Box<Service>>) -> HttpHandler {
+        let mut ct = vec![];
+        for s in &services {
+            ct.append(&mut s.__rpc_list_supported_codecs());
+        }
+        ct.dedup();
         HttpHandler {
-            services: services
+            services: services,
+            content_types: ct,
         }
     }
 }
 
 impl Handler for HttpHandler {
     fn handle<'a, 'k>(&'a self, req: Request<'a, 'k>, mut res: Response<'a, Fresh>) {
+        // first check method
         if req.method != Method::Post {
             make_method_not_allowed_error(res, req.method);
             return
         }
-        
+        // then check content-type
+        if !req.headers.has::<ContentType>() {
+            make_content_type_error("rpc: missing Content-Type header", res);
+            return
+        }
+        // check is content-type is accepted by one of the services
+        let ct = &req.headers.get::<ContentType>().unwrap();
+        if !self.content_types.contains(ct) {
+            return make_content_type_error(&format!("rpc: unrecognized Content-Type, {}", ct), res);
+        }
+
+        // then call the services to execute the method
+        for s in &self.services {
+            s
+        }
     }
+}
+
+fn make_content_type_error<'a,>(body: &str, mut res: Response<'a, Fresh>) {
+    res.headers_mut().set(ContentLength(body.len() as u64));
+    *res.status_mut() = ::hyper::status::StatusCode::BadRequest;
+    let mut res = res.start().unwrap();
+    let _ = res.write_all(body.as_bytes());
 }
 
 fn make_method_not_allowed_error<'a,>(mut res: Response<'a, Fresh>, method: Method) {
