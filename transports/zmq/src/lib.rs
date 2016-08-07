ef06b8900a123bdd7148823b5d60304b7bc3da43
jeremyletang <letang.jeremy@gmail.com>
2016-08-07 20:01:55 +0100

    add base projet for zmq transport

---
+++ transports/zmq/src/lib.rs
@@ -0,0 +1,19 @@
+// Copyright 2016 Jeremy Letang.
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+#![feature(custom_derive, plugin, integer_atomics)]
+
+#[macro_use]
+extern crate log;
+extern crate rpc;
+extern crate zmq;
+
+mod client;
+mod server;
+
+// pub use server::ZmqServerTransport;
+// pub use client::ZmqClientTransport;
