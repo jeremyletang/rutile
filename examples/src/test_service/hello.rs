5f8cc47a52df49e7d4d78d705f7fd6222fcd68c9
jeremyletang <letang.jeremy@gmail.com>
2016-08-08 18:56:43 +0100

    fix typo

--- examples/src/test_service/hello.rs
+++ examples/src/test_service/hello.rs
@@ -1,5 +1,5 @@
 #![rpc_service(MsgpCodec = "::msgp_codec::MsgpCodec",
-               JsonCode = "::json_codec::JsonCodec")]
+               JsonCodec = "::json_codec::JsonCodec")]
 
 use rpc::Context;
 
